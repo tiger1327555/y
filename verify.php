a2dd296d870118a3d794450c7ff3c42c602421ab
tiger1327555 <58632489+tiger1327555@users.noreply.github.com>
2019-12-24 20:54:29 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'lWyRSf6OBf2/is4IsBfpqKqSqSMljaqSHcpBf8ujmSRkIq2J8XF8moroz5cOPUdg5GsoT1pf1evsrFDWsppn33lnP0DIAToZNvnToDXoiHAZiaLa7QF3u/wVoBfNQjf0fYeBQ6D6hYQXP84f+/JdVgdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
